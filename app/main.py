2f5b262746a180a5335acb0cdf0a31914275dae7
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2024-05-08 23:04:05 -0300

    Dockerize Fast API

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# Multiplataforma
+
 app = FastAPI()
 
 
@@ -11,3 +11,4 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
